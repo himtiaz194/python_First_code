1e87c3ddfebebf8e36d5e1bc915d1d116830e99a
[h.imtiaz194] <[h.imtiaz194@gmail.com]>
2019-11-01 02:17:44 +0500

    First Code with 5 build in fuctions

---
+++ p1.py
@@ -0,0 +1,7 @@
+print("Name:Hassan Imtiaz")
+print("Father Name: Imtiaz Ahmad")
+print("contact no: 03408872194")
+print("email: h.imtiaz194@gmail.com")
+print("degree: post-gradute")
+print("love H")
+
